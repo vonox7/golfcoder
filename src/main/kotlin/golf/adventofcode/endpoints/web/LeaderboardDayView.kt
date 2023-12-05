58dde4a4f0194e060aeac0ef6a0f2e872cde4ff8
Valentin Slawicek <valentin.slawi@gmail.com>
2023-12-05 19:40:01 +0100

    Leaderboard static html

--- src/main/kotlin/golf/adventofcode/endpoints/web/LeaderboardDayView.kt
+++ src/main/kotlin/golf/adventofcode/endpoints/web/LeaderboardDayView.kt
@@ -2,17 +2,37 @@ package golf.adventofcode.endpoints.web
 
 import io.ktor.server.application.*
 import io.ktor.server.plugins.*
-import kotlinx.html.h1
-import kotlinx.html.p
+import kotlinx.html.*
 
 object LeaderboardDayView {
     suspend fun getHtml(call: ApplicationCall) {
         val year = 2000 + (call.parameters["year"]?.toIntOrNull() ?: throw NotFoundException("Invalid year"))
         val day = call.parameters["day"]?.toIntOrNull() ?: throw NotFoundException("Invalid day")
 
-        call.respondHtmlView("Advent of Code $year/day/$day") {
-            h1 { +"Advent of Code $year/day/$day" }
-            p { +"Hello, world!" }
+        call.respondHtmlView("Advent of Code Golf Leaderboard $year/day/$day") {
+            h1 { +"Advent of Code Golf Leaderboard $year/day/$day" }
+            table("leaderboard") {
+                thead {
+                    tr {
+                        th {}
+                        th(classes = "left-align") { +"Username" }
+                        th(classes = "left-align") { +"Language" }
+                        th(classes = "right-align") { +"Tokens" }
+                        th(classes = "right-align") { +"Date" }
+                    }
+                }
+                tbody {
+                    (1..10).forEach { rank ->
+                        tr {
+                            td("rank") { +"$rank" }
+                            td { +"My_username" }
+                            td { +"Python" }
+                            td("right-align") { +"1234" }
+                            td("right-align") { +"Two days ago" }
+                        }
+                    }
+                }
+            }
         }
     }
 }
--- src/main/kotlin/golf/adventofcode/endpoints/web/LeaderboardYearView.kt
+++ src/main/kotlin/golf/adventofcode/endpoints/web/LeaderboardYearView.kt
@@ -2,6 +2,7 @@ package golf.adventofcode.endpoints.web
 
 import io.ktor.server.application.*
 import io.ktor.server.plugins.*
+import kotlinx.html.a
 import kotlinx.html.h1
 import kotlinx.html.p
 
@@ -10,8 +11,12 @@ object LeaderboardYearView {
         val year = 2000 + (call.parameters["year"]?.toIntOrNull() ?: throw NotFoundException("Invalid year"))
 
         call.respondHtmlView("Advent of Code $year") {
-            h1 { +"Advent of Code $year" }
-            p { +"Hello, world!" }
+            h1 { +"Advent of Code Golf Leaderboard $year" }
+            (1..24).forEach { day ->
+                p {
+                    a("/$year/day/$day") { +"Day $day" }
+                }
+            }
         }
     }
 }
