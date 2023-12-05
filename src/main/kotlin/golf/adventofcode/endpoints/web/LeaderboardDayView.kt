b56a16e20889f0e2c876c45526d9250de1d15485
Valentin Slawicek <valentin.slawi@gmail.com>
2023-12-05 19:44:18 +0100

    Leaderboard static html

--- src/main/kotlin/golf/adventofcode/endpoints/web/LeaderboardDayView.kt
+++ src/main/kotlin/golf/adventofcode/endpoints/web/LeaderboardDayView.kt
@@ -11,25 +11,34 @@ object LeaderboardDayView {
 
         call.respondHtmlView("Advent of Code Golf Leaderboard $year/day/$day") {
             h1 { +"Advent of Code Golf Leaderboard $year/day/$day" }
-            table("leaderboard") {
-                thead {
-                    tr {
-                        th {}
-                        th(classes = "left-align") { +"Username" }
-                        th(classes = "left-align") { +"Language" }
-                        th(classes = "right-align") { +"Tokens" }
-                        th(classes = "right-align") { +"Date" }
-                    }
+
+            h2 { +"Part 1" }
+            renderLeaderboardTable()
+
+            h2 { +"Part 2" }
+            renderLeaderboardTable()
+        }
+    }
+
+    private fun HtmlBlockTag.renderLeaderboardTable() {
+        table("leaderboard") {
+            thead {
+                tr {
+                    th {}
+                    th(classes = "left-align") { +"Username" }
+                    th(classes = "left-align") { +"Language" }
+                    th(classes = "right-align") { +"Tokens" }
+                    th(classes = "right-align") { +"Date" }
                 }
-                tbody {
-                    (1..10).forEach { rank ->
-                        tr {
-                            td("rank") { +"$rank" }
-                            td { +"My_username" }
-                            td { +"Python" }
-                            td("right-align") { +"1234" }
-                            td("right-align") { +"Two days ago" }
-                        }
+            }
+            tbody {
+                (1..10).forEach { rank ->
+                    tr {
+                        td("rank") { +"$rank" }
+                        td { +"My_username" }
+                        td { +"Python" }
+                        td("right-align") { +"1234" }
+                        td("right-align") { +"Two days ago" }
                     }
                 }
             }
