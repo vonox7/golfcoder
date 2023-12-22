b963db69884ff815a273fde379fd71066f588cb2
Valentin Slawicek <valentin.slawi@gmail.com>
2023-12-22 11:15:30 +0100

    Fix relative time calculation

--- src/main/kotlin/org/golfcoder/utils/DateExtensions.kt
+++ src/main/kotlin/org/golfcoder/utils/DateExtensions.kt
@@ -5,7 +5,7 @@ import java.util.*
 val Date.relativeToNow: String
     get() {
         var difference = (Date().time - time) / 1000
-        val unitMap = mapOf("second" to 60, "minute" to 60, "hour" to 24, "day" to 7, "month" to 12, "year" to 10000)
+        val unitMap = mapOf("second" to 60, "minute" to 60, "hour" to 24, "day" to 7, "month" to 30, "year" to 10000)
         for ((unit, divider) in unitMap) {
             if (difference < divider * 2) { // show e.g. 2h to 48h
                 return "$difference $unit${if (difference > 1) "s" else ""} ago"
