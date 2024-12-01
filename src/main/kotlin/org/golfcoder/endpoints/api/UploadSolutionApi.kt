70198aefb58616d48687a453bd73d9eb2cded0dc
Valentin Slawicek <valentin.slawi@gmail.com>
2024-12-01 19:47:34 +0100

    Fix logging

--- src/main/kotlin/org/golfcoder/endpoints/api/UploadSolutionApi.kt
+++ src/main/kotlin/org/golfcoder/endpoints/api/UploadSolutionApi.kt
@@ -68,7 +68,7 @@ object UploadSolutionApi {
         val tokenizer = request.language.tokenizer
         val tokens: List<Tokenizer.Token>
         val tokenCount: Int
-        print("Tokenize for user ${userSession?.userId}: ${request.year}/${request.day}/${request.part} ${request.language}")
+        println("Tokenize for user ${userSession?.userId}: ${request.year}/${request.day}/${request.part} ${request.language}:\n${request.code}")
         try {
             tokens = tokenizer.tokenize(request.code)
             if (Sysinfo.isLocal) {
@@ -147,7 +147,7 @@ object UploadSolutionApi {
         }
 
         // Run code
-        print("Run code for user ${userSession.userId}: ${request.year}/${request.day}/${request.part} ${request.language}")
+        println("Run code for user ${userSession.userId}: ${request.year}/${request.day}/${request.part} ${request.language}")
         runCode(call, request, expectedOutput, userSession, tokenCount, tokenizer)
     }
 
