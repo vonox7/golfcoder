656690290b0734327864ec111880671e468b860e
Valentin Slawicek <valentin@slawicek.com>
2025-12-14 20:05:20 +0100

    Fix KateAggregator

--- src/main/kotlin/org/golfcoder/expectedoutputaggregator/KateAggregator.kt
+++ src/main/kotlin/org/golfcoder/expectedoutputaggregator/KateAggregator.kt
@@ -13,9 +13,10 @@ class KateAggregator : ExpectedOutputAggregator {
     override suspend fun load(year: Int, day: Int): ExpectedOutputAggregator.AggregatorResult {
         val source = ExpectedOutput.Source.KATE
 
+        val fileSuffix = if (year >= 2024) "ts" else "js"
         val file = httpClient.get(
             "https://raw.githubusercontent.com/katemihalikova/advent-of-code/refs/heads/latest/$year/" +
-                    "${String.format("%02d", day)}.js"
+                    "${String.format("%02d", day)}.$fileSuffix"
         ).takeIf { it.status == HttpStatusCode.OK }?.bodyAsText() ?: run {
             return Failure.NotYetAvailable
         }
