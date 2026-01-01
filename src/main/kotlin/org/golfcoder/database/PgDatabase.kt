6db3597caf842d14ad258cd1a85dea3dc3ac6191
Valentin Slawicek <valentin@slawicek.com>
2026-01-01 13:25:31 +0100

    Migrate PG via exposed MigrationUtils

--- src/main/kotlin/org/golfcoder/database/PgDatabase.kt
+++ src/main/kotlin/org/golfcoder/database/PgDatabase.kt
@@ -2,8 +2,12 @@ package org.golfcoder.database
 
 import io.ktor.http.*
 import io.r2dbc.spi.ConnectionFactoryOptions
+import org.golfcoder.database.pgpayloads.ExpectedOutputTable
+import org.jetbrains.exposed.v1.migration.r2dbc.MigrationUtils
 import org.jetbrains.exposed.v1.r2dbc.R2dbcDatabase
 import org.jetbrains.exposed.v1.r2dbc.R2dbcDatabaseConfig
+import org.jetbrains.exposed.v1.r2dbc.R2dbcTransaction
+import org.jetbrains.exposed.v1.r2dbc.transactions.suspendTransaction
 import org.jetbrains.exposed.v1.r2dbc.transactions.transactionManager
 
 suspend fun connectToPostgres(): R2dbcDatabase {
@@ -28,5 +32,29 @@ suspend fun connectToPostgres(): R2dbcDatabase {
         // Test the connection
         transactionManager.newTransaction().exec("SELECT 1")
         println("PostgreSQL connection successful")
+
+        // Migrate the database
+        suspendTransaction {
+            migrateDb()
+        }
+    }
+}
+
+// We directly migrate everything without flyway for simplicity.
+// This means that in case of errors during migration a manual intervention is required.
+suspend fun R2dbcTransaction.migrateDb() {
+    val tables = arrayOf(
+        ExpectedOutputTable
+    )
+
+    val statements = MigrationUtils.statementsRequiredForDatabaseMigration(*tables)
+
+    if (statements.isEmpty()) {
+        println("No database migrations required")
+    } else {
+        println("Applying database migrations:\n${statements.joinToString("\n")}")
+        statements.forEach {
+            exec(it)
+        }
     }
 }
---
+++ src/main/kotlin/org/golfcoder/database/pgpayloads/ExpectedOutput.kt
@@ -0,0 +1,15 @@
+package org.golfcoder.database.pgpayloads
+
+import org.golfcoder.database.ExpectedOutput
+import org.jetbrains.exposed.v1.core.Table
+
+object ExpectedOutputTable : Table("expectedOutput") {
+    val year = integer("year")
+    val day = integer("day")
+    val part = integer("part")
+    val input = text("input")
+    val output = text("output")
+    val sourceName = enumeration("sourceName", ExpectedOutput.Source::class)
+
+    override val primaryKey = PrimaryKey(year, day, part, sourceName)
+}
